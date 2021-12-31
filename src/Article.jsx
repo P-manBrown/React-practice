85e6e3e9365ff2f61458774ffafcdbaa0bead264
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-01 03:07:16 +0900

    [finish] react-practice3

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LikeButton from "./LikeButton";
 
 const Article = (props) => {
   let isPublishState;
@@ -12,7 +13,7 @@ const Article = (props) => {
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態</label>
       <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}></input>
-
+      <LikeButton count={props.count} />
     </div>
   )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,9 +6,28 @@ class Blog extends React.Component {
     super(props);
     this.state = {
       isPublished: false,
-      order: 1,
+      count: 0,
+    }
+  }
+
+  componentDidMount() {
+    // ボタンがクリックされたらいいねをカウントアップする
+    document.getElementById('counter').addEventListener('click',this.countUp)
+  }
+
+  componentDidUpdate() {
+    if (this.state.count >= 10) {
+      this.setState({
+        count: 0
+      })
     }
   };
+
+  componentWillUnmount() {
+    document.getElementById('counter').removeEventListener('click',this.countUp)
+  }
+
+
   // 公開状態を変更するための関数
   togglePublished = () => {
     this.setState({
@@ -16,13 +35,20 @@ class Blog extends React.Component {
     })
   };
 
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
   render() {
     return(
       <>
         <Article
-          title="hoge"
+          title="Reactの練習"
           isPublished={this.state.isPublished}
           toggle={() => this.togglePublished()}
+          count={() => this.state.count}
         />
       </>
     )
