8584006691ce258a637d1e577bd524ad61f94cd4
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-05 17:19:49 +0900

    [Finish] modernJS-practice3-4

--- jsToReact/src/index.js
+++ jsToReact/src/index.js
@@ -12,7 +12,6 @@ console.log(val1);
 var val1 = "var変数を再宣言";
 console.log(val1);
 
-
 // var変数は意図しない上書きや再宣言が発生する可能性があるため、以下を使用するようになった
 
 // let変数
@@ -72,3 +71,62 @@ console.log(message1);
 // テンプレート文字列を用いた方法
 const message2 = `私の名前は${name}です。年齢は${age}です。`;
 console.log(message2);
+
+
+// アロー関数
+// 従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1が出力されています"));
+// "func1が出力されています"
+
+// 関数を変数に格納してから使用する
+const func2 = function(str) {
+  return str;
+}
+console.log(func2("func2が出力されています"));
+// "func2が出力されています"
+
+// アロー関数
+const func3 = (str) => {
+  return str;
+}
+console.log(func3("func3が出力されています"))
+// "func3が出力されています"
+// アロー関数の注意点
+// 引数が一つの場合には引数の()は省略することができる。
+// 単一式の場合returnを省略し、以下のように記述することができる
+const func4 = (str) => str;
+
+const func5 = ( num1, num2 ) => num1 + num2;
+console.log(func5( 30, 40 ));
+
+
+// 分割代入
+
+// 分割代入を使用しない場合
+const myProfile = {
+  name2: "田中",
+  age2: 40,
+};
+const message3 = `名前は${myProfile.name2}です。年齢は${myProfile.age2}です。`;
+console.log(message3);
+// 名前は田中です。年齢は40です。
+
+// 分割代入を使用することで上記のmyProfileを省略することができる
+const { name2, age2 } = myProfile;
+const message4 = `名前は${name2}です。年齢は${age2}です。`;
+console.log(message4);
+// 名前は田中です。年齢は40です。
+// 分割代入を使用することで記述を簡潔にすることができる
+
+// 分割代入は配列でも使用することができる
+const myProfile2 = [ '田中', 40 ];
+const message5 =  `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}です。`;
+console.log(message5);
+
+// 分割代入は配列でも使用することができる
+const [ name3, age3 ] = myProfile2;
+const message6 = `名前は${name3}です。年齢は${age3}です。`;
+console.log(message6);
